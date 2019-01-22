849969e05464983616c099fc47e37d796f02da98
Ananmay Suri <anchitsuri@Ananmays-MacBook-Air.local>
2019-01-22 16:13:22 +0530

    created imu class for reading data from serial

--- ahrs8p/ahrs8p.py
+++ ahrs8p/ahrs8p.py
@@ -1,43 +1,21 @@
-#!/usr/bin/env python
-
-#Import any external libraries here
 import serial
-import pika
-import asvprotobuf.sensor_pb2
-import time
-import sys
-import asvmq
 
-class IMUSerial(object):
-    def __init__(self, port="/dev/ttyUSB0", topic_name="/imu_raw", queue_size=1000):
-        self.port = serial.Serial(port, baudrate=115200)
-        self.publisher = asvmq.Publisher(topic_name, asvprotobuf.sensor_pb2.Raw, hostname="localhost", port=4369)
+class Imu:
+    def __init__(self, port):
+        self._port_name = port
+        self._port=None
 
-    def query_data(self):
-        self.port.write("quaternion di. accelp di. gyrop di. temperature di.\r\n")
+    def connect(self):
+        self._port=serial.Serial("/dev/tty.usbserial-A5055UXR", baudrate=115200)
+        return self._port.is_open
 
-
-        lines = []
-        for i in range(100):
-            line = self.port.readline()
-            if "OK" in line:
-
-                msg = asvprotobuf.sensor_pb2.Raw()
-                t=time.time()
-                msg.header.stamp.seconds = int(t)
-                msg.header.stamp.nanos=(t%1)*(10**9)
-                msg.header.frame_id = "base_link"
-                msg.data = "".join(lines)
-                return msg
-            lines.append(line)
-        return asvprotobuf.sensor_pb2.Raw()
-
-    def publish(self):
-        while True:
-            self.publisher.publish(self.query_data())
-
-
-if __name__=="__main__":
-    port = sys.argv[1]
-    ser = IMUSerial(port=port)
-    print(ser.query_data())
+    def read(self):
+        if (not self._port or not self._port.is_open):
+            self.connect()
+        self._port.write("roll di. pitch di. yaw di. accelp di. gyrop di. temperature di.\r\n".encode())
+        result = []
+        while(True):
+            line=self._port.readline().decode().strip()
+            if(line.startswith("OK")):
+                return "\n".join(result)
+            result.append(line)
