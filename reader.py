a5f39ff75eed66d510269aef650590ec932d7cb6
Akash Purandare <akash.p1997@gmail.com>
2019-04-23 19:21:15 +0530

    Modified reader to have autodetect

--- reader.py
+++ reader.py
@@ -3,27 +3,28 @@ import asvmq
 import asvprotobuf
 import time
 import datetime
+import asv_logging
 
 try:
     READ_FREQUENCY = 20
 
-    port_name = ahrs8p.get_ahrs()
-    if(port_name==''):
-        asvmq.log_fatal("IMU Module not found.")
-        raise ModuleNotFoundError("IMU Module not found.")
+    #port_name = ahrs8p.get_ahrs()
+    #if(port_name==''):
+    #    asvmq.log_fatal("IMU Module not found.")
+    #    raise ModuleNotFoundError("IMU Module not found.")
     pub = asvmq.Publisher(topic_name="imu_raw", object_type=asvprotobuf.sensor_pb2.Serial)
-    ahrs = ahrs8p.Imu(port_name)
-    ahrs.connect()
+    #ahrs = ahrs8p.Imu(port_name)
+    #ahrs.connect()
     while True:
         t = time.time()
         msg = asvprotobuf.sensor_pb2.Serial()
         msg.header.stamp.seconds = int(t)
         msg.header.stamp.nanos = int((t-int(t))*10**9)
         msg.header.frame_id = "imu"
-        msg.data = ahrs.read()
+        #msg.data = ahrs.read()
         pub.publish(msg)
 
 except KeyboardInterrupt:
-    ahrs.disconnect()
+    #ahrs.disconnect()
     asvmq.log_debug("Closing AHRS Port at %s" % datetime.datetime.now().strftime("%c"))
     print("\nClosing AHRS Port at %s" % datetime.datetime.now().strftime("%c"))
